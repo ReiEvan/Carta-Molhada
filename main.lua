a70fed0230e0cf5cbadd2551a21895a73aa4d77f
Bohemiancoder-jpm <jeronimoquarentena@gmail.com>
2025-11-20 20:04:14 -0300

    +mapa Co-authored-by: Evan <ReiEvan@users.noreply.github.com>

--- main.lua
+++ main.lua
@@ -3,9 +3,9 @@ local button = require "Button"
 
 local game = {
     state = {
-        menu = true,
+        menu = false,
         paused = false,
-        running = false,
+        running = true,
         ended = false,
         
     }
@@ -45,11 +45,16 @@ end
 function love.update(dt)
     player.x, player.y = love.mouse.getPosition()
 end
+-- Carregamento do mapa
+local mapa = love.graphics.newImage("sprites/mapagradeado.png")
 
 function love.draw()
     love.graphics.printf("FPS: " .. love.timer.getFPS(), love.graphics.newFont(16), 10, love.graphics.getHeight() - 30, love.graphics.getWidth())
 
     if game.state["running"] then
+        love.graphics.circle("fill", player.x, player.y, player.radius)
+        -- Desenhar mapa As coordenadas x crescem para a direita e y para baixo
+        love.graphics.draw(mapa, 0, 0, 0, .35, .35)
         love.graphics.circle("fill", player.x, player.y, player.radius)
     elseif game.state["menu"] then
         buttons.menu_state.play_game:draw(10, 20, 10, 20)
