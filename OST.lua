76d5c5262e513283c4f8b22d3e8d8e4932c3a78f
Bohemiancoder-jpm <jeronimoquarentena@gmail.com>
2025-11-27 09:43:36 -0300

    musica oficial

--- OST.lua
+++ OST.lua
@@ -1,7 +1,7 @@
 local love = require "love"
 
 local sounds= {}
-sounds.music = love.audio.newSource('musicas/rascunho trilha padrao.mp3','stream')
+sounds.music = love.audio.newSource('musicas/João Cabral - Musica das agua- 5 Nov 2025, 1105 (online-audio-converter.com).mp3','stream')
 sounds.music:setLooping(true)
 local function somteste()
     sounds.music:play()
