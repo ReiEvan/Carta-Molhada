a02859d204505ef14cf660c69f587472a991ef46
Evan <evasconcelos381@gmail.com>
2026-05-27 11:34:52 -0300

    Trava na orientação da tela para os celulares

--- conf.lua
+++ conf.lua
@@ -1,13 +1,20 @@
 function love.conf(t)
+    t.identity = "UltimaGota"
+    t.version = "11.5"
+
     t.window.title = "Última Gota"
     t.window.width = 1280
     t.window.height = 720
     --Parte importante pro mobile
+    t.window.x = 100
+    t.window.y = 100
     t.modules.joystick = true
     t.modules.audio = true
     t.modules.touch = true
     t.window.fullscreen = false
     t.window.resizable = true
-    --Orientação da tela, nesse caso horizontal
+    --Escala da tela, importante para mobile
     t.window.usedpiscale = true
+    --Orientação da tela, nesse caso horizontal
+    t.window.displayorientation = "landscape"
 end
