1f2cc3000ca042ed1ba9047f9b4d294cd39c949c
Evan <evasconcelos381@gmail.com>
2026-08-19 18:19:40 -0300

    pop-up de fatos nas bandeiras

---
+++ conquistas.lua
@@ -0,0 +1,10 @@
+local love = require("love")
+local Conquistas = {}
+
+--Caminho do arquivo de save persistente
+local ARQUIVO_SAVE = "conquistas_save.txt"
+
+--Lista de conquistas e dados educativos
+Conquistas.lista = {
+    
+}
